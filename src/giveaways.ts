98d3ebba919b39638a1093c9efa41f4c9f4742da
Zilong Zhan <zolong.zhan@gmail.com>
2025-02-19 23:20:50 +0100

    Feat: add user login function

--- src/giveaways.ts
+++ src/giveaways.ts
@@ -3,3 +3,20 @@ import { programData, saveData } from "./storage.js";
 
 import { Giveaway } from "./types.js";
 import { askUserNewGiveawayData } from "./ui.js";
+
+export const loginUser = (email: string, password: string): void => {
+  const currentUser = programData.users.find(
+    (user) => email === user.email && password === user.password
+  );
+
+  if (!currentUser) {
+    console.error("Compruebe los credenciales e introdúzcalos de nuevo");
+    process.exit();
+  }
+
+  currentUser.isAdmin
+    ? (programData.isAdmin = true)
+    : (programData.isAdmin = false);
+
+  programData.userEmail = email;
+};
